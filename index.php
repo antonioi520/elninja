cfb8035c2ff59ac2ede876e37a51cba8418554e6
antonio <prodoson@gmail.com>
2020-05-05 19:54:41 -0400

    added montserrat font

--- index.php
+++ index.php
@@ -22,13 +22,13 @@
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="img/Interior/MiAlmaCafeOpeningCeremony-22.jpg" class="d-block w-100" alt="..." style="height:59.375em;filter: brightness(25%) grayscale(80%);object-fit: cover;">
-                <div class="carousel-caption d-md-block" id="hero" style="top:30%;">
+                <div class="carousel-caption d-md-block" id="hero" style="top:27%;">
                     <center>
                         <h1 class="headertext" style="margin-bottom: 0">Welcome To</h1><br>
 <!--                        <h1 class="headertext shadow">EL NINJA</H1>-->
                     </center>
                     <center>
-                        <img src="img/logo_ninja.png" class="imglogohead" style="margin-bottom: 150px;">
+                        <img src="img/logo_ninja.png" class="imglogohead" style="margin-bottom: 00px;">
                     </center>
                     <center>
                         <a href="#contact" id="box" style="margin-top: 1em;display:block">
