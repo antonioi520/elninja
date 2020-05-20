ff1b0deb8ecc239437fa57177ea448d217025592
Luciano728 <lpm72896@gmail.com>
2020-05-20 14:57:53 -0400

    update services carousel image

--- services.php
+++ services.php
@@ -49,7 +49,7 @@
 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" style="top:-25px;">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img class="d-block w-100" src="img/TheaterInterior/concert-37.jpg" alt="Second slide" style="height:600px;filter:brightness(50%);object-fit: cover">
+            <img class="d-block w-100" src="img/Interior/7.jpg" alt="Second slide" style="height:600px;filter:brightness(50%);object-fit: cover">
             <div class="carousel-caption d-md-block d-flex justify-content-center"  id="hero">
 
                 <center>
