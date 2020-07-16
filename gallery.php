4972a8ccb93405e7678dc1e127187449833fba04
antonio <prodoson@gmail.com>
2020-07-15 22:54:59 -0400

    google analytics gtag

--- gallery.php
+++ gallery.php
@@ -10,14 +10,15 @@
 <html>
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90995944-4"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90995944-6"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'UA-90995944-4');
+        gtag('config', 'UA-90995944-6');
     </script>
+
 
     <meta charset="UTF-8">
     <title>El Ninja Restaurant</title>
